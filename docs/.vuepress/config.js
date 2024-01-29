48a99906307a23092e420caa179e73237fb819b1
Lady-Gao <1440374646@qq.com>
2024-01-29 09:55:08 +0800

    dist/docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ export default defineUserConfig({
       'link', { rel: 'icon', href: 'https://hstsp.com/favicon.ico' }
     ]
   ],
- 
+  dest:"dist/docs",
   theme: defaultTheme({
     home:'/',
     // 在这里进行配置
